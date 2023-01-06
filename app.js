00143a75076f4fb03be50ffad678358fc851764b
orucmu <mustafaoruc720@gmail.com>
2023-01-06 22:32:51 +0300

    Model and routes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
+mongoose.set('strictQuery', false)
+
 const app = express();
+
+//Connect db
+mongoose.connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connected successfuly');
+});
 
 //Template Engine
 app.set("view engine", "ejs");
@@ -10,6 +23,7 @@ app.use(express.static("public"));
 
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
