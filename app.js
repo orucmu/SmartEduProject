4656a9e19c2c233510bd55a7918a1385e652e6c6
orucmu <mustafaoruc720@gmail.com>
2023-01-07 16:20:34 +0300

    postman

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 
@@ -20,6 +21,8 @@ app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.use('/', pageRoute);
