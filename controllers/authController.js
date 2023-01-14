7d24579ee113031a71f508611cb8ad6711b4ccb0
orucmu <mustafaoruc720@gmail.com>
2023-01-14 16:03:00 +0300

    Kişiye Özel İçerik

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ exports.loginUser = (req, res) => {
                     if (same) {
                         // User Session
                         req.session.userID = user._id //userID oluşturmak
-                        res.status(200).redirect('/')
+                        res.status(200).redirect('/users/dashboard');
                     }
                 })
             }
@@ -42,4 +42,12 @@ exports.logoutUser = (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
     })
-}
+};
+
+exports.getDashboardPage = async (req, res) => {
+    const user = await User.findOne({ _id: req.session.userID })
+    res.status(200).render('dashboard', {
+        page_name: "dashboard",
+        user
+    });
+};
