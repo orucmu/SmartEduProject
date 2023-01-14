2b9ca512709fb2a5c780a67c09fdcd28e0d179d2
orucmu <mustafaoruc720@gmail.com>
2023-01-14 16:45:38 +0300

    Özel Middleware

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,4 +1,5 @@
 const pageController = require('../controllers/pageController');
+const redirectMiddleware = require('../middlewares/redirectMiddleware');
 
 const express = require('express')
 
@@ -6,7 +7,7 @@ const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
-router.route('/register').get(pageController.getRegisterPage);
-router.route('/login').get(pageController.getLoginPage);
+router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
+router.route('/login').get(redirectMiddleware, pageController.getLoginPage);
 
 module.exports = router;
