d4a2b4df39b46e3f2de30384a5b8c21b4192935a
orucmu <mustafaoruc720@gmail.com>
2023-01-16 22:27:44 +0300

    Search Alanı

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -22,14 +22,29 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
     try {
         const categorySlug = req.query.categories;
+        const query = req.query.search;
         const category = await Category.findOne({ slug: categorySlug });
 
         let filter = {};
+
         if (categorySlug) {
             filter = { category: category._id }
         }
 
-        const courses = await Course.find(filter).sort('-createdAt');
+        if (query) {
+            filter = { name: query }
+        }
+
+        if (!query && !categorySlug) {
+            filter.name = "",
+                filter.category = null
+        }
+        const courses = await Course.find({
+            $or: [
+                { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+                { category: filter.category }
+            ]
+        }).sort('-createdAt').populate('user');
         const categories = await Category.find();
 
         res.status(200).render('courses', {
@@ -49,10 +64,12 @@ exports.getCourse = async (req, res) => {
     try {
         const user = await User.findById(req.session.userID);
         const course = await Course.findOne({ slug: req.params.slug }).populate('user');
+        const categories = await Category.find();
         res.status(200).render('course', {
             course,
             page_name: 'courses',
-            user
+            user,
+            categories
         });
     } catch (error) {
         res.status(400).json({
